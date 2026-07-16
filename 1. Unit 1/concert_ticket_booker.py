b3e6df26f9b068a0d1f3c1a1ad56a19cf0d5a9d5
Phurulogang Mogau <phurulogang@gmail.com>
2026-07-16 22:08:51 +0200

    Add files via upload

---
+++ 1. Unit 1/concert_ticket_booker.py
@@ -0,0 +1,8 @@
+#collectiong user details
+name= input("Please enter your name: ")
+surname= input("Please enter your surname: ")
+favourite_band= input("Please enter your favourite band: ")
+#combining name and surname
+names= f"{name} {surname}"
+#dispaying booking details
+print(f"Hey {names}! your tickets for {favourite_band} have been booked successfully. See you at the concert!")
